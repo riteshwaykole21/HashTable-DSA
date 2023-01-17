43ecf0782cc1711ae0c3bd1430e7ba89ac9c71dc
“riteshwaykole21” <riteshwaykole21@gmail.com>
2023-01-17 16:26:24 +0530

    [Ritesh]add : UC3removed a single word

--- HashTables/Program.cs
+++ HashTables/Program.cs
@@ -11,9 +11,7 @@ namespace HashTables
         public static void CountNumbOfOccurence(string paragraph)
         {
             MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
-
             string[] words = paragraph.Split(' ');
-
             foreach (string word in words)
             {
                 if (hashTabe.Exists(word.ToLower()))
@@ -28,8 +26,9 @@ namespace HashTables
         {
             Console.WriteLine("Welcome to the Demo Program on HashTables in DSA .\n");
             Console.WriteLine("Please sslect any one from below given options :\n" +
-                "1 : Checking frequency of occurence for a single line statement (UC1).\n" +
-                "2 : Checking Fequancy Of Occurances of word In a paragraph (UC2).\n");
+                "1 : Checking frequency of occurence of words in a single line statement (UC1).\n" +
+                "2 : Checking frequency of occurence of words in a paragraph (UC2).\n" +
+                "3 : Removing word \"avoidable\" and recounting the occurrences in a same paragraph.\n");
             int option = Convert.ToInt32((string)Console.ReadLine());
             switch (option)
             {
@@ -37,22 +36,42 @@ namespace HashTables
                     string paragraph = "To be or not to be";//UC1 (Counting frequency of occurrence of words in a sentence .
                     CountNumbOfOccurence(paragraph);
                     break;
-                case 2:
-                    string paragraph1 = "Paranoids are not Paranoids " +
-                        "because they are mot paranoid" +
-                        "beacuse they keep putting themselves" +
-                        "deliberately into paranoid avoidable" +
-                        "situtaions";
+                case 2:                     //UC2 (Counting frequency of occurrence of words in a paragraph .
+                    string paragraph1 = "Paranoids are not paranoid " +
+                        "because they are paranoid but " +
+                        "because they keep putting themselves " +
+                        "deliberately into paranoid avoidable " +
+                        "situations";
                     CountNumbOfOccurence(paragraph1);
                     break;
-                    default:
-                    Console.WriteLine("Invalid Opration Selected  ! Please try again .");
+                case 3:                     //UC3 (Counting frequency of occurrence of words in a paragraph and removing 1 word and showing difference.
+                    MyMapNode<string, int> hashTabe = new MyMapNode<string, int>(6);
+                    string paragraph2 = "Paranoids are not paranoid " +
+                        "because they are paranoid but " +
+                        "because they keep putting themselves " +
+                        "deliberately into paranoid avoidable " +
+                        "situations";
+                    string[] words = paragraph2.Split(' ');
+                    foreach (string word in words)
+                    {
+                        if (hashTabe.Exists(word.ToLower()))
+                            hashTabe.Add(word.ToLower(), hashTabe.Get(word.ToLower()) + 1);
+                        else
+                            hashTabe.Add(word.ToLower(), 1); //to,1 
+                    }
+                    Console.WriteLine("All words counted .");
+                    hashTabe.Display();
+                    string s = "avoidable";
+                    hashTabe.Remove(s);
+                    Console.WriteLine();
+                    Console.WriteLine($"A word \"{s}\" has been removed ");
+                    hashTabe.Display();
                     break;
-
+                default:
+                    Console.WriteLine("Invalid option selected ! Please try again .");
+                    break;
             }
             Console.ReadLine();
-
         }
-        
     }
 }
